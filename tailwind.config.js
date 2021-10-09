84fa1e1ed396d3bcfc17ef8b9aba0172a9753f9c
Bryan Ray <bryan@bryanray.net>
2021-10-08 20:23:16 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['layouts/**/*.html'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['even']
+    },
+  },
+  plugins: [],
+}
